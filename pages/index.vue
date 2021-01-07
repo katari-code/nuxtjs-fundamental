20766e86ad59bf5a5edc9766bd10bc1324e7e2df
Ibrahim katari <ibrahim.katary@gmail.com>
2021-01-07 11:22:34 +0200

    Creating Our Homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Hello word</div>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: Left; margin: 10px"
+    >
+      <home-card :home="home"></home-card>
+    </div>
+  </div>
 </template>
+
+<script>
+import homes from "../data/homes.json";
+export default {
+  components: { HomeCard },
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
