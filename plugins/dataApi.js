34399d30235c1b4f85da75e129df0b4c5c5423d7
Ibrahim katari <elsaid@live.utm.my>
2021-01-08 07:51:50 +0200

    adding error vaildation

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,18 +1,38 @@
 export default function (context, inject) {
     const appId = "I631M47KD6"
     const apiKey = "c88a4f2828c5bd3ecd9fdd60ab7d187d"
-
+    const headers = {
+        "X-Algolia-API-Key": apiKey,
+        "X-Algolia-Application-Id": appId,
+    }
     inject('dataApi', {
         getHome,
     })
     async function getHome(homeId) {
-        const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/home/${homeId}`, {
-            headers: {
-                "X-Algolia-API-Key": apiKey,
-                "X-Algolia-Application-Id": appId,
-            }
-        })
+        try {
+            return unWrap(await fetch(`https://${appId}-dsn.algolia.net/1/indexes/home/${homeId}`, { headers }))
+        } catch (error) {
+            return getErrorResponse(error)
+        }
+    }
+
+    async function unWrap(response) {
         const json = await response.json()
-        return json
+        const { ok, status, statusText } = response
+        return {
+            json,
+            ok,
+            status,
+            statusText,
+        }
+    }
+
+    function getErrorResponse(error) {
+        return {
+            ok: false,
+            status: 500,
+            statusText: error.message,
+            json: {}
+        }
     }
 }
